fca358e84a955bef4c3dc91fd1a0b203d4ace8ce
GabCB <gabycuencas@gmail.com>
2023-03-20 16:54:37 +0100

    create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,30 @@
+export const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.image} alt="the movie poster" />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Image: </span>
+        <span>{movie.image}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
