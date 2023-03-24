77fe8390e8142d48790616b966c0f900df470ffd
GabCB <gabycuencas@gmail.com>
2023-03-24 09:44:22 +0100

    update login-view.jsx

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+
+export const LoginView = () => {
+  const [username, setUnsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    //this prevents the default behaviour of the form which is to reload the entire page
+    event.preventDefault();
+
+  const data = {
+    Username: username,
+    Password: password
+  };
+
+  fetch ("https://moviewebapp.herokuapp.com/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Login response: ", data);
+    if (data.user) {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", data.token);
+      onLoggedIn(data.user, data.token);
+    } else {
+      alert("No such user");
+    }
+  })
+  .catch((e) => {
+    alert("Something went worng. Please try again.");
+  });
+}
+
+return (
+  <form>
+    <label>
+      Username:
+      <input type="text"/>
+    </label>
+    <label>
+      Password:
+      <input type="password"/>
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+);
+};
