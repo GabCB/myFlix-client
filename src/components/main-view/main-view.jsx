b4a67894693480e43faf4c6a1f945a87a8702e58
GabCB <gabycuencas@gmail.com>
2023-03-27 17:21:06 +0200

    add routes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,8 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
-import { Col, Button, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -27,7 +28,81 @@ export const MainView = () => {
       }, [token]);
 
   return (
-    <Row className="justify-content-md-center" style={{border: "1px solid red"}}>
+    <BrowserRouter>
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                <Col md={5}>
+                  <SignupView />
+                </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+        <Route
+            path="/movies/:movieId"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>No movies to show</Col>
+                ) : (
+                  <Col md={8}>
+                    <MovieView movies={movies} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+        <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>No movies to show</Col>
+                ) : (
+                  movies.map((movie) => (
+                    <Col className="mb-4" key={movie._id} md={3}>
+                      <MovieCard movie={movie} />
+                    </Col>
+                  ))
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
+  );
+};
+
+//export default MainView;
+  /*return (
+    <BrowserRouter>
+      <Row className="justify-content-md-center" style={{border: "1px solid red"}}>
       {!user ?(
         <Col md={5} className="text-danger">
           <h1 className="text-danger">My Flix</h1>
@@ -95,6 +170,7 @@ export const MainView = () => {
           ))}
         </>
       )}
-    </Row>
-  );
-};
+      </Row>
+    </BrowserRouter>
+  );*/
+
