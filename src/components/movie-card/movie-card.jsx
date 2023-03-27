6ff0aa4e34320a3b229ec25538e15993524b04f1
GabCB <gabycuencas@gmail.com>
2023-03-27 17:21:53 +0200

    replace onMovieClick and onBackClick

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,18 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100"
-      onClick={() =>{
-        onMovieClick(movie);
-      }}
-    >
-      <Card.Img variant="top" src={movie.image} />
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.imagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Director.Name}</Card.Text>
-        <Button 
-          onClick={() => {
-            onMovieClick(movie);
-          }}
-        >
-          Open
-        </Button>
+        <Link to ={ `/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -27,9 +21,8 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 //here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired
-//    image: PropTypes.string.isRequired,
-//    director: PropTypes.string
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};
+ };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,49 @@
-export const MovieView = ({ movie, onBackClick }) => {
+import { Button, Col, Row } from "react-bootstrap";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import "./movie-view.scss";
+
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+  const movie = movies.find((m) => m._id === movieId);
+
   return (
     <div>
       <div>
-        <img src={movie.ImagePath} alt="the movie poster" />
+        <div>
+          < img className ="w-100" src={movie.imagePath} alt="the movie poster" />
+        </div>
+        <div>
+          <span className="text-danger">Title: </span>
+          <span className="text-danger">{movie.Title}</span>
+        </div>
+        <div>
+          <span className="text-danger">Description: </span>
+          <span className="text-danger">{movie.Description}</span>
+        </div>
+        <div>
+          <span className="text-danger">Genre: </span>
+          <span className="text-danger">{movie.Genre.Name}</span>
+        </div>
+        <div>
+          <span className="text-danger">Director: </span>
+          <span className="text-danger">{movie.Director.Name}</span>
+        </div>
+        <Link to={`/`}>
+          <button className="back-button">Back</button>
+        </Link>
       </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.Title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.Description}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.Genre.Name}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.Director.Name}</span>
-      </div>
-      <button onClick={onBackClick}>Back</button>
+      <br />
+      <h2 className="text-danger">Similar movies</h2>
+      <Row>
+        {movies
+        .filter((m) => m.genre.name === movie.Genre.Name)
+        .map((m) => (
+          <Col md={6} key={movie._id}>
+            <MovieCard movie={m} />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
