294caeda6d3408d59a79d5de577754ed6ec73571
GabCB <gabycuencas@gmail.com>
2023-03-27 11:26:26 +0200

    remove min.css

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { MainView} from "./components/main-view/main-view";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
+import { Container } from "react-bootstrap";
 
 //Main component (will eventually use all the others)
 const App = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 // Main component (will eventually use all the others)
