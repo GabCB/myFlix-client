b67a068c929dd03c169405302d2af1220ec2eef7
GabCB <gabycuencas@gmail.com>
2023-03-27 12:04:02 +0200

    replace quotation marks with backticks

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -18,7 +18,7 @@ export const MainView = () => {
       return;
     }
     fetch("https://moviewebapp.herokuapp.com/movies", {
-      headers: { Authorization: "Bearer ${token"},
+      headers: { Authorization: `Bearer ${token}`, }
     })
       .then((response) => response.json())
       .then((movies) => {
