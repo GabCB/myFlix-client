e07ec2a78e8056c9ff0e9edc0f71a59c757cb8e4
GabCB <gabycuencas@gmail.com>
2023-03-28 12:31:48 +0200

    correct syntax

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -10,7 +10,7 @@ export const MovieCard = ({ movie }) => {
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Director.Name}</Card.Text>
-        <Link to ={ `/movies/${encodeURIComponent(movie.id)}`}>
+        <Link to ={ `/movies/${encodeURIComponent(movie._id)}`}>
           <Button variant="link">Open</Button>
         </Link>
       </Card.Body>
