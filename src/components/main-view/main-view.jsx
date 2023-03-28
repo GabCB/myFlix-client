19bffc8f40384e2c0b974763c87bd0b7cf900a0e
GabCB <gabycuencas@gmail.com>
2023-03-28 14:08:28 +0200

    add nav bar

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { Col, Row } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
@@ -29,6 +30,7 @@ export const MainView = () => {
 
   return (
     <BrowserRouter>
+    <NavigationBar user={user} onLoggedOut={() => {setUser(null); }} />
       <Row className="justify-content-md-center">
         <Routes>
           <Route
