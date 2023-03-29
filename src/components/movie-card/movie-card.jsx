1daad8d8e1b27893995ea1fb9071561c2cf47b63
GabCB <gabycuencas@gmail.com>
2023-03-29 12:20:20 +0200

    change text

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,15 +3,16 @@ import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+
 export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Card.Text>Director: {movie.Director.Name}</Card.Text>
         <Link to ={ `/movies/${encodeURIComponent(movie._id)}`}>
-          <Button variant="link">Open</Button>
+          <Button variant="link">More</Button>
         </Link>
       </Card.Body>
     </Card>
