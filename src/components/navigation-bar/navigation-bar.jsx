26c53757d4179681d590c87de44840e5715bba07
GabCB <gabycuencas@gmail.com>
2023-03-29 12:27:58 +0200

    change text "Home" to "Movies"

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -24,7 +24,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
             {user && (
               <>
                 <Nav.Link as={Link} to="/">
-                  Home
+                  Movies
                 </Nav.Link>
                 <Nav.Link as={Link} to="/profile">
                   Profile
