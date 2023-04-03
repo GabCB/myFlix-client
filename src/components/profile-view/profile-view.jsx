f17d9f7d03bdbdd0705b3785045d62b66541ea1a
GabCB <gabycuencas@gmail.com>
2023-04-03 13:42:06 +0200

    update profile-view jsx file

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,58 +1,27 @@
-import { useState, useEffect } from "react"; 
-import { MovieCard } from "../movie-card/movie-card";
+import PropTypes from "prop-types";
+import { Row, Col, Container, Card, Button } from "react-bootstrap";
 import { UpdateUser } from "./update-user";
 import { FavoriteMovies } from "./favorite-movies";
-import { Link } from "react-router-dom";
-import { Button, Row, Col, Container, Form, Card } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import "./profile-view.scss";
 
-//import { Link } from "react-router-dom";
-export const ProfileView = ({user, movies}) => {
-  const storedUser = localStorage.getItem("user");
-  const storedToken = localStorage.getItem("token");
-  const storedMovies = JSON.parse(localStorage.getItem("movies"));
-
-  const [token] = useState(storedToken ? storedToken: null);
-  const [username, setUsername] = useState(user.Username);
-  const [password, setPassword] = useState(user.Password);
-  const [email, setEmail] = useState(user.Email);
-  const [birthday, setBirthday] = useState(user.Birthday);
-  const [favoriteMovies, setFavoriteMovies] = useState ([]);
-  const [allMovies] = useState(storedMovies ? storedMovies: movies );
-  const [filteredMovies, setFilteredMovies] = useState ([]);
-
-  //show updated user on the profile
-  const getUser = (token) => {
-    fetch(`https://moviewebapp.herokuapp.com/users/${user.Username}`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-  }).then(response => response.json())
-  .then ((response) => {
-    console.log("getUser response", response);
-    setUsername(response.Username);
-    setEmail(response.Email);
-    setPassword(response.Password);
-    setBirthday(response.Birthday);
-    setFavoriteMovies(response.FavoriteMovies)
-  })
-}
-console.log("userFavoriteMovie", favoriteMovies)
-
-useEffect(() => {
-  getUser(token);
-}, [])
-
-
-  return (
-    <Container>
+function ProfileView ({
+  movies,
+  removeMovie,
+  user
+}) {
+ 
+return (
+<Container>
       <Row>
         <Col>
           <Card>
             <Card.Body>
               <div>
                 <h4>User Information</h4>
-                <p>Username: {username}</p>
-                <p>Birthday: {birthday}</p>
-                <p>e-mail: {email}</p>
+                <p>Username: {user.Username}</p>
+                <p>Birthday: {user.Birthday.slice(0, 10)}</p>
+                <p>e-mail: {user.Email}</p>
               </div>
             </Card.Body>
           </Card>
@@ -68,6 +37,38 @@ useEffect(() => {
       <Row>
         <FavoriteMovies user={user} movies={movies} />
       </Row>
+      <Row>
+        <div>
+          <Button
+            className="back-button"
+            as={Link}
+            to={"/"}
+          >
+            Back
+          </Button>
+        </div>
+      </Row>
     </Container>
   )
 };
+
+export { ProfileView}; 
+
+ProfileView.propTypes= {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.isRequired,
+      Genre: PropTypes.shape ({
+        Name: PropTypes.string.isRequired,
+      }).isRequired,
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired
+      }).isRequired,
+      ImagePath: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
