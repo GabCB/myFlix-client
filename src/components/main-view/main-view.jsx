8f5c58b35c109cccb4d1c56ac96d4e9c94d2c732
GabCB <gabycuencas@gmail.com>
2023-04-03 13:44:11 +0200

    add search function

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,7 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { ProfileView } from "../profile-view/profile-view";
+import { SearchBar } from "../search-bar/search-bar";
 import { Col, Row } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
@@ -13,8 +14,10 @@ export const MainView = () => {
   const storedToken = localStorage.getItem("token");
   const [user, setUser] = useState(null);
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [filteredMovies, setFilteredMovies] = useState(null);
   const [token, setToken] = useState(null);
+  const [userQuery, setUserQuery] = useState(null);
+    
 
   useEffect(() => {
     if (!token) {
@@ -29,14 +32,39 @@ export const MainView = () => {
         });
       }, [token]);
 
+  const onSearch = function (searchInput) {
+    setUserQuery(searchInput);
+  };
+
+  useEffect(
+    function () {
+      if (!userQuery) {
+        setFilteredMovies([]);
+      } else {
+        let searchResult = movies.filter(function(movie) {
+          const movieLowerCase= movie.Title.toLowerCase();
+          const directorLowerCase = movie.Director.Name.toLowerCase();
+          const genreLowerCase= movie.Genre.Name.toLowerCase();
+          const userQueryLowerCase = userQuery.toLowerCase();
+
+          return (
+            movieLowerCase.includes(userQueryLowerCase) ||
+            directorLowerCase.includes(userQueryLowerCase) ||
+            genreLowerCase.includes(userQueryLowerCase)
+          );
+        });
+        setFilteredMovies(searchResult);
+      }
+    }, [movies, userQuery]);
+
   return (
     <BrowserRouter>
     <NavigationBar user={user} onLoggedOut={() => {setUser(null); setToken(null); localStorage.clear(); }} />
-      <Row className="justify-content-md-center">
-        <Routes>
-          <Route
-            path="/signup"
-            element={
+<Row className="justify-content-md-center">
+  <Routes>
+    <Route
+path="/signup"
+element={
               <>
                 {user ? (
                   <Navigate to="/" />
@@ -84,16 +112,48 @@ export const MainView = () => {
             path="/"
             element={
               <>
+                <SearchBar onSearch={onSearch} />
                 {!user ? (
                   <Navigate to="/login" replace />
-                ) : movies.length === 0 ? (
-                  <Col>No movies to show</Col>
-                ) : (
-                  movies.map((movie) => (
-                    <Col className="mb-4" key={movie._id} md={3}>
+                ) : userQuery && filteredMovies.length === 0 ? (
+                  <Col className= "mt-5">
+                  Sorry, we could not find any movies that match your search.
+                  Please try again with a different term.
+                  </Col>
+                ) : userQuery ? (
+                  <>
+                  {filteredMovies.map(function(movie) {
+                    return (
+                      <Col
+                      className="mb-4"
+                      key={movie._id}
+                      xs={6}
+                      md={4}
+                      lg={3}
+                      xl={2}
+                      >
                       <MovieCard movie={movie} />
-                    </Col>
-                  ))
+                      </Col>
+                    );
+                  })}
+                  </>
+                ) : (
+                  <>
+                  {movies.map(function(movie) {
+                    return (
+                      <Col
+                        className="mb-4"
+                        key={movie._id}
+                        xs={6}
+                        md={4}
+                        lg={3}
+                        xl={2}
+                      >
+                        <MovieCard movie={movie} />
+                      </Col>
+                    );
+                  })}
+                  </>
                 )}
               </>
             }
