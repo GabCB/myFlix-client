a13dc6178151480ae7ec5a5d156e070c41865fef
GabCB <gabycuencas@gmail.com>
2023-04-03 13:42:43 +0200

    update favorite-movie jsx file

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,64 +1,61 @@
-import { useState, useEffect } from "react"; 
+import PropTypes from "prop-types";
+import { Col } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
-import { ProfileView} from "../profile-view/profile-view";
-import { Button, Col, Card, Link } from "react-bootstrap";
 
-export const FavoriteMovies = ({user, movies}) => {
-  const storedUser =  JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
-  const storedMovies = JSON.parse(localStorage.getItem("movies"));
+function FavoriteMovies ({movies, removeMovie, user}) {
+  let favoriteMovies = movies.filter (function(movie) {
+    return user.FavoriteMovies.includes(movie._id);
+  });
+  let printFavoriteMovies;
 
-  const [token] = useState(storedToken ? storedToken: null);
-  const [username, setUsername] = useState (null);
-  const [password, setPassword] = useState (null);
-  const [email, setEmail] = useState (null);
-  const [birthday, setBirthday] = useState (null);
-  const [favoriteMovies, setFavoriteMovies] = useState ([]);
-  const [allMovies] = useState(storedMovies ? storedMovies: movies );
-  const [filteredMovies, setFilteredMovies] = useState ([]);
+  if (favoriteMovies.length === 0) {
+    printFavoriteMovies = (
+      <Col className="mt-4">You have not added any movies yet</Col>
+    );
+  } else {
+    printFavoriteMovies = favoriteMovies.map(function(movie) {
+      return (
+        <Col>
+          <MovieCard
+          isfavMovieCard = {true}
+          movie={movie}
+          removeMovie={removeMovie}
+          />
+        </Col>
+      );
+    });
+  }
+  return <>
+  <h4>Favorite movies</h4>
+  {printFavoriteMovies}
+  </>;
 
-const getUser = (token) => {
-  fetch(`https://moviewebapp.herokuapp.com/users/${user.Username}`, {
-    method: "GET",
-    headers: { Authorization: `Bearer ${token}` },
-  }).then(response => response.json())
-  .then ((response) => {
-  console.log("getUser response", response);
-  setUsername(response.Username);
-  setEmail(response.Email);
-  setPassword(response.Password);
-  setBirthday(response.Birthday);
-  setFavoriteMovies(response.FavoriteMovies)
-})
-console.log("userFavoriteMovie", favoriteMovies)
-
-const favoriteMovies = movies.filter((movie) => favoriteMovies.includes(movie._id));
-
-console.log("favoriteMovies", favoriteMovies)
-
-useEffect(() => {
-  const newList = allMovies.filter((movie) => {
-    const hasMovieId = favoriteMovies.some ((m) => movie._id === m);
-    if (hasMovieId) {
-      return movie
-    }
-  })
-  setFavoriteMovies (newList)
-  getUser(token);
-}, [])
-
-return (
-  <>
-    <h4>Favorite movies</h4>
-    {favoriteMovies.length === 0 ? 
-    <span>No movies selected</span> : favoriteMovies.map((movie) => (
-    <Col className="mb-4" key={movie._id} xs={12} md={6} lg={3}>
-      <MovieCard movie={movie} />
-    </Col>
-     ))
-   }
-  </>
-)
-
- }
 }
+ export {FavoriteMovies};
+
+FavoriteMovies.propTypes= {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.isRequired,
+      Genre: PropTypes.shape ({
+        Name: PropTypes.string.isRequired,
+      }).isRequired,
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired
+      }).isRequired,
+      ImagePath: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  removeMovie: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+    Birthday: PropTypes.string.isRequired,
+    FavoriteMovies: PropTypes.array,
+  }).isRequired,
+};
