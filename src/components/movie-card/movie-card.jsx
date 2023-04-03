d55e8c24b7aacc7fb1e963b981be7cb32e5089a8
GabCB <gabycuencas@gmail.com>
2023-04-03 13:43:40 +0200

    update movie-card jsx file

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
-      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Img variant="top" src={movie.ImagePath} alt={movie.Title} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Director.Name}</Card.Text>
@@ -21,8 +21,8 @@ export const MovieCard = ({ movie }) => {
 //here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Image: PropTypes.string.isRequired,
     director: PropTypes.string.isRequired
   }).isRequired,
  };
